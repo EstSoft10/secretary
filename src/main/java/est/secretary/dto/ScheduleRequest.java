23383f44b2ca4c0c61ef03145a67d21af973c979
hy <hyeyunn01@naver.com>
2025-06-03 14:14:01 +0900

    feature(calendar) : 1. DTO 수정

--- src/main/java/est/secretary/dto/ScheduleRequest.java
+++ src/main/java/est/secretary/dto/ScheduleRequest.java
@@ -10,7 +10,6 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 public class ScheduleRequest {
-	private Long memberId;
 	private String title;
 	private String content;
 	private LocalDateTime start;
