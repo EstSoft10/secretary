9a8783f0cd3bdc6be1d5a3a5487c87fd4a1df96f
박동재 <156689148+ehdwo13@users.noreply.github.com>
2025-06-14 21:23:11 +0900

    Update start.sh

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,4 +1,3 @@
-# scripts/start.sh
 #!/bin/bash
 echo "✅ Spring Boot 시작 중..."
-nohup java -jar /home/ec2-user/app.jar > /dev/null 2>&1 &
+nohup java -Djasypt.encryptor.password=$JASYPT_ENCRYPTOR_PASSWORD -jar /home/ec2-user/app/app.jar > /home/ec2-user/app/app.log 2>&1 &
